ecfc758b5f48e8d743e7e1f383f786ab4b60ffb0
chauhana1013 <100536433+chauhana1013@users.noreply.github.com>
2022-03-02 13:00:07 +1300

    Add files via upload

---
+++ 02_variables.py
@@ -0,0 +1,16 @@
+# code to learn about variables
+
+# variables can be a string
+from xml.etree.ElementTree import PI
+
+
+greeting = "Hi there"
+print(greeting)
+
+# they can also be numbers
+fav_num = 8
+
+# Below is a constant (the nam is in UPPERCASE)
+PI = 3.14
+
+num_2 = 2
